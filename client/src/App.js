44245e3f2ad0c4f3477476b4d2a6d072b1db1c86
copernichris <christhomas2101@gmail.com>
2021-11-06 16:54:06 -0400

    commit 2

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,9 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
-
+import StoreProvider from './utils/storeProvider';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
